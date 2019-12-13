8754e2884a078cd23f40104edbf608b801d56345
Peeter Vahe vahepeeter@gmail.com <peeter.vahe@hotmail.com>
2019-12-13 15:19:34 +0200

    Fixed days

--- tests/unit/Date.spec.js
+++ tests/unit/Date.spec.js
@@ -3,10 +3,10 @@ import Header from "../../src/components/Header";
 
 describe('HeaderCorrect', () => {
 
-    const wrapper = mount(Header)
+    const wrapper = mount(Header);
 
     let monthnames = ["JAN","FEB","MAR","APR","MAY","JUNE","JUL","AUG","SEP","OCT","NOV","DEC"];
-
+    let days = ["SUDNAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
     it('Check if Header displays correct things', ()=> {
         let today = new Date();
         let date = today.getDate() < 10 ?  `0${today.getDate()}` : today.getDate();
@@ -16,7 +16,7 @@ describe('HeaderCorrect', () => {
         expect(wrapper.html()).toContain(year);
         expect(wrapper.html()).toContain(date);
         expect(wrapper.html()).toContain(monthnames[month]);
-        expect(wrapper.html()).toContain(weekday)
+        expect(wrapper.html()).toContain(days[weekday])
     })
 
 });
