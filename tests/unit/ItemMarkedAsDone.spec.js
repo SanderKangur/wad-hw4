ffa79a32c6890c96d8a99758d5e73632827ceed9
alvarantson <alvarantson@gmail.com>
2019-12-14 15:19:23 +0200

    TASK 4 #2

--- tests/unit/ItemMarkedAsDone.spec.js
+++ tests/unit/ItemMarkedAsDone.spec.js
@@ -1,10 +1,8 @@
-import { mount } from '@vue/test-utils'
+import { mount, Vue } from '@vue/test-utils'
 import List from "../../src/components/List";
 import Item from "../../src/models/Item";
 
-
 describe('NoListItem', () => {
-
     let list = [];
 
     let testItem = new Item("TASK");
@@ -15,10 +13,13 @@ describe('NoListItem', () => {
             list: list
         }
     });
-	wrapper.find("span").trigger("click");
 
-    it('Test to test when an item in the list is marked as done  item is updated correctly', () => {
+    it('Test to test when an item in the list is marked as done item is updated correctly', async () => {
+
+    	wrapper.find("span").trigger("click");
+  		await Vue.nextTick() //This should update but doesnt
         expect(list[0].done).toEqual(true);
+        console.log(wrapper.html())
         expect(wrapper.findAll(".list-item.done").length).toEqual(1);
         
     })
